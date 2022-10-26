bd51ec27240e1f50ba91794d1b5921e4bd37a790
khalid fifel <fifle.khalid99@gmail.com>
2022-10-27 00:34:59 +0100

    claimer code of data base

--- database.php
+++ database.php
@@ -1,10 +1,11 @@
 <?php
     
     //CONNECT TO MYSQL DATABASE USING MYSQLI
-    $conn = mysqli_connect("localhost", "root", "", "Youcodescumboard");
+    $conn = mysqli_connect("localhost", "root", "", "youcodescumboard");
     // Check connection
     if ($conn -> connect_errno) {
         echo "Failed to connect to MySQL: " . $conn -> connect_error;
         exit();
     }
+    return $conn;
 ?>
